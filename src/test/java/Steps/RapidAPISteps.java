dbcb00b1f8f900b774249da12cb7745e0b13a353
Esteban Peña <esteban.pena@fitideas.co>
2022-05-30 18:25:05 -0500

    30-5

--- src/test/java/Steps/RapidAPISteps.java
+++ src/test/java/Steps/RapidAPISteps.java
@@ -1,4 +1,67 @@
 package Steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
 public class RapidAPISteps {
+
+    private RequestSpecification requestSpecification;
+    private Response response;
+    private ValidatableResponse json;
+    private String accessToken;
+
+
+    @Given("^I have a valid API key for the (.+) URI$")
+    public void APIKey(String URI){
+        requestSpecification = given()
+                .header("X-RapidAPI-Host","bravenewcoin.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "89f67d1c99msh67a6f772cc246a6p107ba0jsn94b856abd0c0")
+                .baseUri(URI).contentType(ContentType.JSON)
+                .log().all();
+    }
+
+    @When("^I send a POST request with a valid body to the endpoint (.+)$")
+    public void sendPOSTRequest(String endpoint){
+        /*JSONParser jsonParser = new JSONParser();
+        Object jsonData = jsonParser.parse(new FileReader("src/test/sources/payLoads/TokenGettableJson.json"));
+        JSONObject json = (JSONObject) jsonData;*/
+
+        JSONObject json = new JSONObject();
+        json.put("audience", "https://api.bravenewcoin.com");
+        json.put("grant_type","client_credentials");
+        json.put("client_id", "oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY");
+
+        response = requestSpecification.when().body(json.toString()).post(endpoint).prettyPeek();
+    }
+
+    @Then("^I can validate I received a valid token in the response$")
+    public void validateValidToken(){
+        json = response.then().statusCode(200);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/sources/features/RapidAPI.feature
+++ src/test/sources/features/RapidAPI.feature
@@ -3,3 +3,21 @@
 
     Rule: When I send a POST request, I receive a token I can use it to make further authenticated calls.
 
+      @FIRST
+    Scenario: As a user a can retrieve a Token when making a valid post request
+      Given I have a valid API key for the https://bravenewcoin.p.rapidapi.com URI
+      When I send a POST request with a valid body to the endpoint /oauth/token
+      Then I can validate I received a valid token in the response
+
+    Scenario: As a user, when I can use an invalid API key, I get a HTTP Code Status 403
+      Given I have an invalid API key for the https://bravenewcoin.p.rapidapi.com URI
+      When I send a POST request with a valid body to the endpoint /oauth/token
+      Then I can validate I received a HTTP Code Status 403
+
+    Scenario: As a user, when I can send the request without "grand_type", I get a HTTP Code Status 400
+      Given I have a valid API key for the https://bravenewcoin.p.rapidapi.com URI
+      When I send a POST request with an invalid body to the endpoint /oauth/token
+      Then I can validate I received a HTTP Code Status 400
+
+
+
